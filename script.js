99610840b399875611fe1f9f7a43e80210dc24ee
phmanhkhoee <phmanhkhoee@gmail.com>
2024-11-16 17:12:42 +0700

    add source

---
+++ script.js
@@ -0,0 +1,21 @@
+let fiterItem = document.querySelector('.items-links');
+let fileteImages = document.querySelectorAll('.project-img');
+
+
+window.addEventListener('load', () => {
+    fiterItem.addEventListener('click', (selectedItem) => {
+        if (selectedItem.target.classList.contains('item-link')) {
+            document.querySelector('.menu-active').classList.remove('menu-active');
+            selectedItem.target.classList.add('menu-active');
+            let filterName = selectedItem.target.getAttribute('data-name');
+            fileteImages.forEach((image) => {
+                let fileteImages = image.getAttribute('data-name');
+                if ((fileteImages == filterName) || filterName == 'all') {
+                    image.style.display = 'block'
+                } else {
+                    image.style.display = 'none'
+                }
+            })
+        }
+    })
+})
