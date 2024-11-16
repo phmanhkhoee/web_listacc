140a3f6770d8a6d2ade22814b6715c9a102ebe1a
emCam <118101223+phmanhkhoee@users.noreply.github.com>
2024-11-16 20:49:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let fiterItem = document.querySelector('.items-links');
 let fileteImages = document.querySelectorAll('.project-img');
 
-
+// Xử lý lọc ảnh khi bấm nút
 window.addEventListener('load', () => {
     fiterItem.addEventListener('click', (selectedItem) => {
         if (selectedItem.target.classList.contains('item-link')) {
+            // Đổi trạng thái nút active
             document.querySelector('.menu-active').classList.remove('menu-active');
             selectedItem.target.classList.add('menu-active');
+            
+            // Lấy data-name từ nút được chọn
             let filterName = selectedItem.target.getAttribute('data-name');
+            
+            // Lọc ảnh
             fileteImages.forEach((image) => {
-                let fileteImages = image.getAttribute('data-name');
-                if ((fileteImages == filterName) || filterName == 'all') {
-                    image.style.display = 'block'
+                let imageName = image.getAttribute('data-name');
+                if (filterName === 'all' || imageName === filterName) {
+                    image.style.display = 'block';
                 } else {
-                    image.style.display = 'none'
+                    image.style.display = 'none';
                 }
-            })
+            });
         }
-    })
-})
+    });
+});
